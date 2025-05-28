5a66cdd28110cc3422c1ddd64b1bea30132b57cb
binhv005 <vtb22522005@gmail.com>
2025-05-28 20:48:35 +0700

    fix student login

--- Forms/Student/frmTongQuan.Designer.cs
+++ Forms/Student/frmTongQuan.Designer.cs
@@ -22,8 +22,8 @@
             this.btnDanhSach = new FontAwesome.Sharp.IconButton();
             this.btnBaiTap = new FontAwesome.Sharp.IconButton();
             this.btnTongQuan = new FontAwesome.Sharp.IconButton();
-            this.lblTitle = new System.Windows.Forms.Label();
             this.lblChao = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,8 +39,9 @@
             this.panelMenu.Controls.Add(this.lblTitle);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(241, 600);
+            this.panelMenu.Size = new System.Drawing.Size(271, 750);
             this.panelMenu.TabIndex = 1;
             // 
             // btnLogOut
@@ -48,9 +49,10 @@
             this.btnLogOut.BackColor = System.Drawing.Color.DarkGray;
             this.btnLogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.btnLogOut.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.btnLogOut.Location = new System.Drawing.Point(6, 553);
+            this.btnLogOut.Location = new System.Drawing.Point(7, 691);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(226, 35);
+            this.btnLogOut.Size = new System.Drawing.Size(254, 44);
             this.btnLogOut.TabIndex = 2;
             this.btnLogOut.Text = "Đăng xuất";
             this.btnLogOut.UseVisualStyleBackColor = false;
@@ -67,9 +69,10 @@
             this.btnDangKy.IconColor = System.Drawing.Color.White;
             this.btnDangKy.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnDangKy.IconSize = 30;
-            this.btnDangKy.Location = new System.Drawing.Point(0, 455);
+            this.btnDangKy.Location = new System.Drawing.Point(0, 577);
+            this.btnDangKy.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(241, 87);
+            this.btnDangKy.Size = new System.Drawing.Size(271, 109);
             this.btnDangKy.TabIndex = 1;
             this.btnDangKy.Text = "Đăng ký học phần";
             this.btnDangKy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -85,9 +88,10 @@
             this.btnDiem.IconColor = System.Drawing.Color.White;
             this.btnDiem.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnDiem.IconSize = 30;
-            this.btnDiem.Location = new System.Drawing.Point(0, 371);
+            this.btnDiem.Location = new System.Drawing.Point(0, 472);
+            this.btnDiem.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDiem.Name = "btnDiem";
-            this.btnDiem.Size = new System.Drawing.Size(241, 84);
+            this.btnDiem.Size = new System.Drawing.Size(271, 105);
             this.btnDiem.TabIndex = 2;
             this.btnDiem.Text = "Xem điểm";
             this.btnDiem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -104,9 +108,10 @@
             this.btnDanhSach.IconColor = System.Drawing.Color.White;
             this.btnDanhSach.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnDanhSach.IconSize = 30;
-            this.btnDanhSach.Location = new System.Drawing.Point(0, 283);
+            this.btnDanhSach.Location = new System.Drawing.Point(0, 362);
+            this.btnDanhSach.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnDanhSach.Name = "btnDanhSach";
-            this.btnDanhSach.Size = new System.Drawing.Size(241, 88);
+            this.btnDanhSach.Size = new System.Drawing.Size(271, 110);
             this.btnDanhSach.TabIndex = 3;
             this.btnDanhSach.Text = "Danh sách khóa học";
             this.btnDanhSach.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -122,9 +127,10 @@
             this.btnBaiTap.IconColor = System.Drawing.Color.White;
             this.btnBaiTap.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnBaiTap.IconSize = 30;
-            this.btnBaiTap.Location = new System.Drawing.Point(0, 195);
+            this.btnBaiTap.Location = new System.Drawing.Point(0, 252);
+            this.btnBaiTap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnBaiTap.Name = "btnBaiTap";
-            this.btnBaiTap.Size = new System.Drawing.Size(241, 88);
+            this.btnBaiTap.Size = new System.Drawing.Size(271, 110);
             this.btnBaiTap.TabIndex = 4;
             this.btnBaiTap.Text = "Bài tập";
             this.btnBaiTap.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -140,12 +146,23 @@
             this.btnTongQuan.IconColor = System.Drawing.Color.White;
             this.btnTongQuan.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnTongQuan.IconSize = 30;
-            this.btnTongQuan.Location = new System.Drawing.Point(0, 110);
+            this.btnTongQuan.Location = new System.Drawing.Point(0, 146);
+            this.btnTongQuan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnTongQuan.Name = "btnTongQuan";
-            this.btnTongQuan.Size = new System.Drawing.Size(241, 85);
+            this.btnTongQuan.Size = new System.Drawing.Size(271, 106);
             this.btnTongQuan.TabIndex = 5;
             this.btnTongQuan.Text = "Thông tin người dùng";
             this.btnTongQuan.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            // 
+            // lblChao
+            // 
+            this.lblChao.AutoSize = true;
+            this.lblChao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblChao.Location = new System.Drawing.Point(573, 11);
+            this.lblChao.Name = "lblChao";
+            this.lblChao.Size = new System.Drawing.Size(95, 36);
+            this.lblChao.TabIndex = 3;
+            this.lblChao.Text = "label1";
             // 
             // lblTitle
             // 
@@ -156,28 +173,19 @@
             this.lblTitle.Image = ((System.Drawing.Image)(resources.GetObject("lblTitle.Image")));
             this.lblTitle.Location = new System.Drawing.Point(0, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(241, 110);
+            this.lblTitle.Size = new System.Drawing.Size(271, 146);
             this.lblTitle.TabIndex = 6;
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // lblChao
-            // 
-            this.lblChao.AutoSize = true;
-            this.lblChao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.lblChao.Location = new System.Drawing.Point(509, 9);
-            this.lblChao.Name = "lblChao";
-            this.lblChao.Size = new System.Drawing.Size(81, 29);
-            this.lblChao.TabIndex = 3;
-            this.lblChao.Text = "label1";
-            // 
             // frmTongQuan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(736, 600);
+            this.ClientSize = new System.Drawing.Size(828, 750);
             this.Controls.Add(this.lblChao);
             this.Controls.Add(this.panelMenu);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmTongQuan";
             this.Text = "frmTongQuan";
             this.panelMenu.ResumeLayout(false);
@@ -189,7 +197,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panelMenu;
-        private System.Windows.Forms.Label lblTitle;
         private FontAwesome.Sharp.IconButton btnDangKy;
         private FontAwesome.Sharp.IconButton btnDiem;
         private FontAwesome.Sharp.IconButton btnDanhSach;
@@ -197,5 +204,6 @@
         private FontAwesome.Sharp.IconButton btnTongQuan;
         private System.Windows.Forms.Button btnLogOut;
         private System.Windows.Forms.Label lblChao;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
